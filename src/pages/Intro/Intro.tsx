a64f25ff1f98273a88e4a5944e9c11e050dd5710
Volodymyr Voronov <malones.vladimir@gmail.com>
2024-02-17 19:23:12 +0100

    Updated Into.

--- src/pages/Intro/Intro.tsx
+++ src/pages/Intro/Intro.tsx
@@ -96,9 +96,9 @@ const Intro = (): JSX.Element => {
       <AnimatePresence>
         {!!tracks.length && (
           <motion.div
-            initial={{ opacity: 0, y: 50 }}
+            initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 50 }}
+            exit={{ opacity: 0, y: 100 }}
             transition={{ duration: 0.5, delay: 0.5 }}
           >
             <Button className={styles["play"]} disabled={uploading}>
